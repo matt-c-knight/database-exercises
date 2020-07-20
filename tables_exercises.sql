e7ea7e7154b482aabbafcfa474e21f3d096f2c0b
Matthew Knight <mattknight.sa@gmail.com>
2020-07-20 14:23:55 -0500

    adding sql

---
+++ tables_exercises.sql
@@ -0,0 +1,20 @@
+SHOW databases;
+
+DESCRIBE employees;
+
+USE employees;
+
+SHOW CREATE TABLE employees;
+
+SHOW tables;
+
+DESCRIBE dept_emp;
+
+DESCRIBE employees;
+
+DESCRIBE salaries;
+
+DESCRIBE dept_manager;
+
+DESCRIBE employees;
+
