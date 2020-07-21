d975600d427bfb293a6af7cb350176bd486df71b
Matthew Knight <mattknight.sa@gmail.com>
2020-07-21 15:24:31 -0500

    updated with questions included

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -60,3 +60,49 @@ FROM employees
 WHERE last_name like '%q%'
 AND last_name not like '%qu%';
 
+Create a new file named order_by_exercises.sql and copy in the contents of your exercise from the previous lesson.
+-- Modify your first query to order by first name. The first result should be Irena Reutenauer and the last result should be Vidya Simmen.
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+order by  first_name;
+
+-- Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+order by  first_name, last_name;
+-- Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+order by  last_name, first_name;
+-- Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
+FROM employees
+WHERE last_name like 'E%'
+AND last_name like '%e'
+order by emp_no;
+
+-- Now reverse the sort order for both queries.
+FROM employees
+WHERE last_name like 'E%'
+AND last_name like '%e'
+order by emp_no desc;
+
+SELECT *
+FROM employees
+WHERE last_name like 'E%'
+OR last_name like '%e'
+order by emp_no desc;
+
+SELECT *
+FROM employees
+WHERE last_name like 'E%'
+AND last_name like '%e'
+order by emp_no desc;
+-- Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+SELECT *
+FROM employees
+WHERE hire_date Between '1990-01-01' and '1999-12-31'
+AND birth_date Like '%12-25';
+order by birth_date asc, hire_date desc;
