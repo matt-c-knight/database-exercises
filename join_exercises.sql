fef7cec43c49dc215fc6fb67320728feadc9febf
Matthew Knight <mattknight.sa@gmail.com>
2020-07-24 10:49:16 -0500

    finished thru 9

--- join_exercises.sql
+++ join_exercises.sql
@@ -69,3 +69,23 @@ Order by average_salary desc
 limit 1;
 
 -- Who is the highest paid employee in the Marketing department?
+SELECT first_name, last_name
+from employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+JOIN salaries ON employees.emp_no = salaries.emp_no
+WHERE dept_emp.to_date > CURDATE() and salaries.to_date > CURDATE() and departments.dept_no = "d001" 
+Group by first_name, last_name
+Order by avg(salary) desc 
+limit 1;
+
+-- Which current department manager has the highest salary?
+SELECT first_name, last_name, salary, dept_name
+FROM departments
+JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+JOIN salaries ON dept_manager.emp_no = salaries.emp_no
+JOIN employees ON salaries.emp_no = employees.emp_no
+WHERE dept_manager.to_date > curdate() and salaries.to_date > curdate()
+Group by first_name, last_name, salary, dept_name
+Order by avg(salary) desc 
+limit 1;
