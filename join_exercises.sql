e24359c2427ad7a83cb4b7478165bc78e16b8b7c
Matthew Knight <mattknight.sa@gmail.com>
2020-07-26 09:50:27 -0500

    finished #5

--- join_exercises.sql
+++ join_exercises.sql
@@ -40,7 +40,7 @@ from titles
 JOIN employees ON titles.emp_no = employees.emp_no
 JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
 JOIN departments ON dept_emp.dept_no = departments.dept_no
-WHERE titles.to_date > curdate() and departments.dept_no = "d009"
+WHERE titles.to_date > curdate() and dept_emp.to_date > curdate() and departments.dept_no = "d009"
 GROUP by titles.title;
 -- Find the current salary of all current managers.
 SELECT dept_name, CONCAT(first_name, " ",last_name), salary
@@ -89,3 +89,95 @@ WHERE dept_manager.to_date > curdate() and salaries.to_date > curdate()
 Group by first_name, last_name, salary, dept_name
 Order by avg(salary) desc 
 limit 1;
+
+--10. Bonus Find the names of all current employees, their department name, and their current manager's name.
+
+-- In DB language: 
+-- 1. get all employees who are CURRENTLY in a department 
+-- (dept_emp, currently -> filter to_date), 
+-- 2. and get their name (employees)
+-- 3. and get their department name (departments)
+-- 4. and add the current manager of each department (dept_manager) (departments, employees)
+-- 5. tie those current managers to the employees' departments 
+
+-- 1. 
+SELECT de.emp_no, de.dept_no
+FROM dept_emp AS de
+WHERE de.to_date > curdate();
+
+-- 2. GET EMP NAMES
+SELECT de.emp_no, e.emp_no, de.dept_no, e.first_name, e.last_name
+FROM dept_emp AS de
+JOIN employees AS e ON de.emp_no = e.emp_no
+WHERE de.to_date > curdate();
+
+-- 3. GET DEPT NAMES
+SELECT de.emp_no, de.dept_no, e.first_name, e.last_name, d.dept_name
+FROM dept_emp AS de
+JOIN employees AS e ON de.emp_no = e.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE de.to_date > curdate();
+
+-- 4. GET CURRENT MANAGER OF EACH DEPARTMENT
+SELECT dm.emp_no, dm.dept_no
+FROM dept_manager dm
+WHERE dm.to_date > curdate();
+
+-- 5. TIE THE MANAGERS DEPARTMENTS WITH THE CURRENT EMPLOYEES DEPARTMENTS, I.E. JOIN
+SELECT de.emp_no
+        , de.dept_no
+        , e.first_name
+        , e.last_name
+        , d.dept_name
+        , dm.emp_no AS mgr_emp_no
+FROM dept_emp AS de
+JOIN employees AS e ON de.emp_no = e.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+JOIN dept_manager AS dm ON de.dept_no = dm.dept_no AND dm.to_date > curdate()
+WHERE de.to_date > curdate();
+
+-- 6. get managers name by joining manager employee number on employees table
+SELECT de.emp_no
+        , de.dept_no
+        , e.first_name
+        , e.last_name
+        , d.dept_name
+        , dm.emp_no AS mgr_emp_no
+        , ee.first_name AS mgr_first
+        , ee.last_name AS mgr_last
+FROM dept_emp AS de
+JOIN employees AS e ON de.emp_no = e.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+JOIN dept_manager AS dm ON de.dept_no = dm.dept_no AND dm.to_date > curdate()
+JOIN employees AS ee ON dm.emp_no = ee.emp_no
+WHERE de.to_date > curdate();
+
+-- 7. clean it up to make it look as requested
+
+SELECT CONCAT(emp.first_name, ' ', emp.last_name) AS employee_name
+        , d.dept_name
+        , CONCAT(manager_emp.first_name, ' ', manager_emp.last_name) AS manager_name
+FROM dept_emp AS de
+JOIN employees AS emp ON de.emp_no = emp.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+JOIN dept_manager AS dm ON de.dept_no = dm.dept_no 
+        AND dm.to_date > curdate()
+JOIN employees AS manager_emp ON dm.emp_no = manager_emp.emp_no
+WHERE de.to_date > curdate();
+
+
+SELECT DISTINCT sub.dept_name
+FROM (
+
+SELECT CONCAT(emp.first_name, ' ', emp.last_name) AS employee_name
+        , d.dept_name
+        , CONCAT(manager_emp.first_name, ' ', manager_emp.last_name) AS manager_name
+FROM dept_emp AS de
+JOIN employees AS emp ON de.emp_no = emp.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+JOIN dept_manager AS dm ON de.dept_no = dm.dept_no 
+        AND dm.to_date > curdate()
+JOIN employees AS manager_emp ON dm.emp_no = manager_emp.emp_no
+WHERE de.to_date > curdate()
+
+) AS sub;
---
+++ subqueries_exercises.sql
@@ -0,0 +1,47 @@
+-- Find all the employees with the same hire date as employee 101010 using a sub-query.
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE hire_date IN (
+	SELECT hire_date
+	FROM employees
+	WHERE emp_no = "101010"
+	);
+-- Find all the titles held by all employees with the first name Aamod.
+SELECT title, COUNT(*)
+FROM titles 
+WHERE emp_no IN (
+	SELECT emp_no
+	from employees
+	WHERE first_name = "Aamod")
+	Group by title;
+
+-- How many people in the employees table are no longer working for the company?
+SELECT CONCAT(first_name, ' ', last_name)
+from employees
+WHERE emp_no IN (
+	SELECT emp_no
+	from dept_emp
+	WHERE to_date != "9999-01-01"
+	)
+
+-- Find all the current department managers that are female.
+SELECT first_name, last_name
+from employees
+WHERE employees.emp_no IN (
+	SELECT employees.emp_no
+	from employees
+	JOIN dept_manager ON employees.emp_no = dept_manager.emp_no 
+	WHERE employees.gender = "F" and dept_manager.to_date > curdate()
+	)
+
+-- Find all the employees that currently have a higher than average salary.
+SELECT first_name, last_name, salary
+FROM employees
+JOIN salaries on employees.emp_no = salaries.emp_no
+WHERE to_date > now()
+    and salary > (select avg(salary)
+                from salaries);
+
+
+-- How many current salaries are within 1 standard deviation of the highest salary?
+
