8ad0929651c0af761032daaac9d4e0b22a59012c
Matthew Knight <mattknight.sa@gmail.com>
2020-07-29 20:08:13 -0500

    working on q1

---
+++ more_exercises.sql
@@ -0,0 +1,13 @@
+-- How much do the current managers of each department get paid, 
+-- relative to the average salary for the department? 
+-- Is there any department where the department manager gets paid less than the average salary?
+
+use employees;
+
+SELECT CONCAT(first_name, " ", last_name) as fullname, salary, dept_name
+FROM dept_manager
+JOIN salaries ON dept_manager.emp_no = salaries.emp_no
+JOIN employees ON salaries.emp_no = employees.emp_no
+JOIN dept_emp ON salaries.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE employees.emp_no = dept_manager.emp_no and salaries.to_date > curdate() and dept_manager.to_date > curdate();
