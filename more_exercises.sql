d10e7c2a7e1de04efcda705a3623e3c337018570
Matthew Knight <mattknight.sa@gmail.com>
2020-08-26 15:13:37 -0500

    finished #2

--- more_exercises.sql
+++ more_exercises.sql
@@ -11,3 +11,19 @@ JOIN employees ON salaries.emp_no = employees.emp_no
 JOIN dept_emp ON salaries.emp_no = dept_emp.emp_no
 JOIN departments ON dept_emp.dept_no = departments.dept_no
 WHERE employees.emp_no = dept_manager.emp_no and salaries.to_date > curdate() and dept_manager.to_date > curdate();
+
+-- Use the world database for the questions below.
+
+-- What languages are spoken in Santa Monica?
+
+SELECT Language, Percentage
+from countrylanguage
+JOIN city on countrylanguage.CountryCode = city.CountryCode
+Where city.Name = "Santa Monica"
+order by percentage;
+
+-- How many different countries are in each region?
+SELECT Region, COUNT(REGION) as num_countries
+from country
+GROUP BY Region
+ORDER BY COUNT(REGION);
